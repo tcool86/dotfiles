d6fc23e461d4b80f5263cdaaa4824aa327038ed8
Timothy Cool <tcoolpg@gmail.com>
2021-03-07 22:16:58 -0800

    simplify profile for use on other machines

--- profile.zsh
+++ profile.zsh
@@ -3,41 +3,8 @@ alias chrome="open -a 'Google Chrome'"
 
 # Development
 alias subl='/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl'
-alias tccss='cd /Applications/XAMPP/htdocs/timcool-site/web/styles/custom-styles; sass --watch custom.scss:../custom.css; subl ../../'
-alias brack='open -a /Applications/Brackets.app'
-alias unreal='/Applications/Epic\ Games\ Launcher.app/Contents/MacOS/EpicGamesLauncher-Mac-Shipping'
 alias vscode='open -a /Applications/Visual\ Studio\ Code.app/ .'
 alias showports='lsof -PiTCP -sTCP:LISTEN'
-alias termtab='iterm_open_tab'
-
-# Utility Functions
-iterm_open_tab() {
-	osascript -e 'tell application "iTerm" to activate' -e 'tell application "System Events" to tell process "iTerm" to keystroke "t" using command down'
-}
-
-iterm_divide_window() {
-	osascript -e 'tell application "iTerm" to activate' -e 'tell application "System Events" to tell process "iTerm" to keystroke "d" using command down'
-}
-
-display_profile_path_fancy() {
-	echo "\n✨ ~/.oh-my-zsh/custom/profile.zsh ✨\n"
-}
-
-change_directory_custom_shell() {
-	cd ~/.oh-my-zsh/custom/
-}
-
-# Project launchers
-launch_za_worldo() {
-	cd ~/Projects/za-worldo/
-	iterm_divide_window
-	iterm_open_tab;
-	cd ~/Projects/za-worldo-client/;
-	iterm_divide_window;
-	npm run serve;
-}
-
-alias zaworldo="launch_za_worldo"
 
 # Grep
 export GREP_OPTIONS='--color=auto'
